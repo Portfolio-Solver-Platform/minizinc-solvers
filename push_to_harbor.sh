68132f6ec5a3245cd6759d2644971ce9b51649fc
Sofus Laubel <sofus@laubel.net>
2025-10-08 22:27:32 +0200

    fix: correct harbor reference path in push_to_harbor.sh

--- push_to_harbor.sh
+++ push_to_harbor.sh
@@ -4,13 +4,13 @@ HARBOR_URL="${HARBOR_URL:-harbor.local}"
 export COSIGN_PASSWORD="lol" # Disclaimer only used for local testing
 META="$(mktemp)"
 
-REF="$HARBOR_URL/psp/minizinc-solver"
+REF="$HARBOR_URL/psp-solvers/minizinc-solver"
 
 docker logout $HARBOR_URL || true
 docker login $HARBOR_URL -u admin -p admin
 
 docker build -t minizinc-solver:latest .
-docker tag minizinc-solver:latest $HARBOR_URL/psp/minizinc-solver:latest
+docker tag minizinc-solver:latest $HARBOR_URL/psp-solvers/minizinc-solver:latest
 
 PUSH_OUT="$(docker push "$REF:latest" 2>&1 | tee /dev/stderr)"
 DIGEST="$(printf "%s\n" "$PUSH_OUT" | awk '/digest:/ {print $3}')"
