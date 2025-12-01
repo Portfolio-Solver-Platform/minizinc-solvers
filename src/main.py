1c678d02d9b4f6b2277ff2fc55a5793502397c5d
sofusl90 <sofus@laubel.net>
2025-12-01 17:37:06 +0100

    sync

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from .minizinc import solve
 from psp_solver_sdk.sat import sat_solver
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 
 
 app = sat_solver("MiniZinc", solve)
