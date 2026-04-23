9fd93d41a2e541c97b8ed2c886a473f7c9f11afb
Sofus Laubel <51532553+sofusl90@users.noreply.github.com>
2026-04-23 18:14:06 +0200

    sync (#22)

--- src/main.py
+++ src/main.py
@@ -5,10 +5,9 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
-
 
 
 app = sat_solver("MiniZinc", solve)
